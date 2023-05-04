eed55f96e7ddfdebb5802a22bba9417afcab89f7
Akshyat Patra <akshyatpatra@gmail.com>
2023-05-04 15:42:35 +0530

    AudioElement UI Updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import './App.css';
-import Songtitles from './components/Songtitles';
-import Moodbuttons from './components/Moodbuttons';
-import Songs from './SongDatabase/songs';
-import Header from './components/Header';
+import { useState } from "react";
+import "./App.css";
+import Songtitles from "./components/Songtitles";
+import Moodbuttons from "./components/Moodbuttons";
+import Songs from "./SongDatabase/songs";
+import Header from "./components/Header";
 function App() {
   const [songUrl, setsongUrl] = useState("");
   const [songName, setsongName] = useState("...");
@@ -17,61 +17,98 @@ function App() {
     setsongName(songTitle);
     setsongUrl(songSource);
     setplayerthumbnail(playerThumbnail);
-  }
+  };
 
-  const toggle = ()=>{
+  const toggle = () => {
     settoggleMode(!toggleMode);
-  }
+  };
 
-  return <div className={toggleMode ? DarkmodeClass.join(" ") : NonDarkModeClass.join("")}>
-    <Header toggleMode={toggleMode} />
-    <div className='audioElement'>
-      <img onClick={toggle} className='playerThumbnail' src={playerthumbnail || "/songplayer/Assets/playerDisc.png"} alt={songName}></img>
-      <p style={toggleMode ? null : {color: "black"}}>{songName}</p>
-      <audio
-        controls autoPlay={true} loop={true}
-        src={songUrl}>
-        Your browser does not support the
-        <code>audio</code> element.
-      </audio>
-    </div>
-
-    <div className='tileContainer'>
-      {
-        Songs.map((song) => {
-          return <Songtitles key={song.SongID}
-            thumbnail={song.Thumbnail}
-            source={song.Source}
-            title={song.Title}
-            category={song.Category}
-            toggleMode={toggleMode}
-            playFunction={playSong} />
-
-        })
+  return (
+    <div
+      className={
+        toggleMode ? DarkmodeClass.join(" ") : NonDarkModeClass.join("")
       }
-    </div>
-    <hr></hr>
-    <Moodbuttons setmood={setmood} toggleMode={toggleMode} />
-    
-    <div className={toggleMode ? "searchBar" : "darkSearchBar"}>
-    <input placeholder='🔍Search your song..' value={searchVal} onChange={(e) => { setsearchVal(e.target.value) }}></input>
+    >
+      <Header toggleMode={toggleMode} />
+      <div className="audioElement">
+        <div class="player">
+          <img
+            onClick={toggle}
+            className="playerThumbnail"
+            src={playerthumbnail || "/songplayer/Assets/playerDisc.png"}
+            alt={songName}
+          ></img>
+          <div class="nameplayer">
+            <p style={toggleMode ? {color: "#0b0b0c"} : { color: "#0b0b0c" }}>{songName}</p>
+            <audio controls autoPlay={true} loop={true} src={songUrl}>
+              Your browser does not support the
+              <code>audio</code> element.
+            </audio>
+          </div>
+        </div>
+      </div>
 
-    </div>
-      <div className='listTileHolder'>
+      <div className="tileContainer">
+        {Songs.map((song) => {
+          return (
+            <Songtitles
+              key={song.SongID}
+              thumbnail={song.Thumbnail}
+              source={song.Source}
+              title={song.Title}
+              category={song.Category}
+              toggleMode={toggleMode}
+              playFunction={playSong}
+            />
+          );
+        })}
+      </div>
+      <hr></hr>
+      <Moodbuttons setmood={setmood} toggleMode={toggleMode} />
 
-        {
-          !searchVal ? Songs.filter(song => song.Category.includes(mood)).map((item, index) =>
-            (<li onClick={() => playSong(item.Source, item.Title, item.Thumbnail)} className='listTiles'>{index + 1}. {item.Title} | {item.Category.toUpperCase()}</li>)) : Songs.filter(song => song.Title.toUpperCase().includes(searchVal.toUpperCase())).map((item, index) =>
-              (<li onClick={() => playSong(item.Source, item.Title, item.Thumbnail)} className='listTiles'>{index + 1}. {item.Title} |{item.Category.toUpperCase()}</li>))
-        }
+      <div className={toggleMode ? "searchBar" : "darkSearchBar"}>
+        <input
+          placeholder="🔍Search your song.."
+          value={searchVal}
+          onChange={(e) => {
+            setsearchVal(e.target.value);
+          }}
+        ></input>
+      </div>
+      <div className="listTileHolder">
+        {!searchVal
+          ? Songs.filter((song) => song.Category.includes(mood)).map(
+              (item, index) => (
+                <li
+                  onClick={() =>
+                    playSong(item.Source, item.Title, item.Thumbnail)
+                  }
+                  className="listTiles"
+                >
+                  {index + 1}. {item.Title} | {item.Category.toUpperCase()}
+                </li>
+              )
+            )
+          : Songs.filter((song) =>
+              song.Title.toUpperCase().includes(searchVal.toUpperCase())
+            ).map((item, index) => (
+              <li
+                onClick={() =>
+                  playSong(item.Source, item.Title, item.Thumbnail)
+                }
+                className="listTiles"
+              >
+                {index + 1}. {item.Title} |{item.Category.toUpperCase()}
+              </li>
+            ))}
+      </div>
+      <div className="footer">
+        <b>Made By Subhranshu</b>
+        <h5>Switch Mode</h5>
+        <input type="checkbox" onClick={toggle}></input>
+      </div>
     </div>
-    <div className='footer'>
-      <b>Made By Subhranshu</b>
-      <h5>Switch Mode</h5>
-      <input type="checkbox" onClick={toggle}></input>
-
-    </div>
-  </div>
+  );
 }
 
 export default App;
